479e2577829eeadccb275b540f71a2722e473cb4
yuenanmu <1730593502@qq.com>
2026-01-30 17:48:08 +0800

    单独创建有别于系统的python虚拟环境

--- WebSpider/Obtain-the-page-source-code/simp_spider.py
+++ WebSpider/Obtain-the-page-source-code/simp_spider.py
@@ -1,5 +1,11 @@
 from urllib.request import urlopen
 
+import sys
+# 打印当前 Python 解释器的路径
+print("当前 Python 路径：", sys.executable)
+# 打印当前环境的 site-packages 目录（库安装位置）
+print("库安装目录：", sys.path[-1])
+
 url="https://www.baidu.com"
 
 resp=urlopen(url)  #返回的是一个 具有属性和方法的特殊对象
@@ -16,3 +22,4 @@ text=data.decode("utf-8")  #解码,str类型，字符串
 #print(text)
 with open("mybaidu.html",mode="w",encoding="utf-8") as f:
     f.write(text)#将内容写入到本地文件
+print("百度首页源码已保存到 mybaidu.html 文件中。")
