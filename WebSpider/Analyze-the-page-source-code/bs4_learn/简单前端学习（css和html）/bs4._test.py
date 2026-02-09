c4c3e1bd6daca843a429ad1cc364d3e3de7515ce
yuenanmu <1730593502@qq.com>
2026-02-09 21:52:28 +0800

    bs4基本语法练习，开始改用logging模块

--- WebSpider/Analyze-the-page-source-code/bs4_learn/简单前端学习（css和html）/bs4._test.py
+++ WebSpider/Analyze-the-page-source-code/bs4_learn/简单前端学习（css和html）/bs4._test.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+import logging
+logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(levelname)s - %(message)s")
 
 html="""
     <ul>
@@ -6,9 +8,26 @@ html="""
         <li id="abc"><a href="zhouxingchi.com">周星驰</a></li>
         <li><a href="zhubajie.com">猪八戒</a></li>
         <li><a href="wuzetian.com">武则天</a></li>
+        <a href="sunwukong.com">孙悟空</a></li>
     </ul>
 """
 #创建BeautifulSoup对象
 soup=BeautifulSoup(html,"html.parser")
 
-soup.prettify()#格式化输出HTML代码
+#logging.debug("GET_HTML:%s\n",soup.prettify())#格式化输出HTML代码,需要占位符
+#print("GET_HTML_22222\n",soup.prettify())
+
+li=soup.find("li",id="abc")#找特定标签,返回一个Tag对象
+logging.debug(type(li))#<class 'bs4.element.Tag'>,返回一个Tag对象
+a=li.find("a")
+logging.debug(a.text)#获取文本内容：周星驰
+logging.debug(a.get("href"))#获取属性内容：zhouxingchi.com
+
+li_list=soup.find_all("li")#找多个标签，返回一个列表，列表中的每个元素都是一个Tag对象
+logging.debug(type(li_list))
+logging.debug(li_list)
+for item in li_list:
+    a=item.find("a")
+    text=a.text
+    href=a.get("href")
+    logging.info("文本内容：%s,链接地址：%s",text,href)
