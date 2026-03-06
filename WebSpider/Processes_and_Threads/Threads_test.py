85f5842b5ae37b37aa020fba6e57306b29ef0009
yuenanmu <1730593502@qq.com>
2026-03-06 18:49:22 +0800

    子线程传入元组参数

--- WebSpider/Processes_and_Threads/Threads_test.py
+++ WebSpider/Processes_and_Threads/Threads_test.py
@@ -3,15 +3,17 @@ import time
 
 def worker():
     start=time.time()
-    for i in range(200):
+    for i in range(20):
         print("worker:",i)
     end=time.time()
     print("Worker thread time:", end - start)
-
+def func(name):
+    for i in range(20):
+        print(name,i)
 class MyThread(Thread):
     def run(self):
         start=time.time()
-        for i in range(200):
+        for i in range(20):
             print("CWorker:",i)
         end=time.time()
         print("CWorker thread time:", end - start)
@@ -20,10 +22,15 @@ if __name__ == "__main__":
     main_start=time.time()
     worker_thread=Thread(target=worker)#创建线程对象，target参数指定线程要执行的函数
     worker_thread.start()#多线程可以开始执行的工作状态，不一定立马开始执行，取决于CPU调度
-    
     cworker_thread=MyThread()#创建线程对象，调用run方法
     cworker_thread.start()
-    for i in range(200):
+    func_thread=Thread(target=func,args=("func_thread",))#传递参数给线程函数，args参数是一个元组，要“，”
+    func_thread.start()
+    #三个线程先并发执行，主线程继续执行下面的代码
+    worker_thread.join()#等待worker_thread线程执行完毕
+    cworker_thread.join()#等待cworker_thread线程执行完毕
+    func_thread.join()
+    for i in range(20):
         print("main:",i)
     main_end=time.time()
     print("Main thread time:", main_end - main_start)
