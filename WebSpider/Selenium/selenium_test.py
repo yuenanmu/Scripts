a2811dc4b1144114b693ecf3a438c7797a5a578b
yuenanmu <1730593502@qq.com>
2026-03-12 22:08:22 +0800

    Selenium练习

---
+++ WebSpider/Selenium/selenium_test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service
+
+driver_path=r"d:\git\scripts\WebSpider\Selenium\chromedriver.exe"#不同电脑需要改文件名！
+service=Service(executable_path=driver_path)#创建Service对象，指定chromedriver.exe的路径
+#Web=Chrome(executable_path="./chromedriver.exe")
+
+Web=webdriver.Chrome(service=service)#初始化Chrome浏览器实例
+url="https://www.baidu.com/"
+
+Web.get(url)
+print(Web.title )
+Web.quit()
